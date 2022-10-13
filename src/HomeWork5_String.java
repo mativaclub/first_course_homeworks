151176e4fff777e9ad940c88ba261c96a872df61
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-10-13 11:23:54 +0300

    first commit

--- src/HomeWork5_String.java
+++ src/HomeWork5_String.java
@@ -1,19 +1,50 @@
+import java.util.Arrays;
+
 public class HomeWork5_String {
 
     public static void main(String[] args) {
 
         //        Homework 5.1
 
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("The name of an employee is - " + fullName);
 
 
+        //        Homework 5.2
+
+        String upperCase = fullName.toUpperCase();
+        System.out.println(upperCase);
 
 
+        //        Homework 5.3
+
+        String nameFormat = fullName.replace(" ", "; ");
+        System.out.println(nameFormat);
 
 
+       //        Homework 5.4.1
+        String fullNameNew = "Иванов Семён Семёнович";
+        String[] splitting = fullNameNew.split("");
+        System.out.println(Arrays.toString(splitting));
+        String replacing = fullNameNew.replace('ё', 'е');
+        System.out.println(replacing);
 
 
+        //        Homework 5.4.2
 
 
+        String fullNameNew1 = "Иванов Семён Семёнович";
+        String[] letterChange = fullNameNew1.split(" ");
+        System.out.print("Данные ФИО сотрудника — ");
+        for (String letter : letterChange) {
+            if (letter.contains("ё")) {
+                letter = letter.replace('ё', 'е');
+            }
+            System.out.print(letter + " ");
+        }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
@@ -8,42 +6,15 @@ public class Main {
 
 
 
-//
-//        String firstName = "Ivan";
-//        firstName = " " + firstName;
-//        String middleName = "Ivanovich";
-//        middleName = " " + middleName;
-//        String lastName = "Ivanov";
-//        String fullName = lastName + firstName + middleName;
-//        System.out.println("ФИО сотрудника — " + fullName);
-//
-//        //**Задание 2**
-//        String firstNameUp = firstName.toUpperCase();
-//        String middleNameUp = middleName.toUpperCase();
-//        String lastNameUp = lastName.toUpperCase();
-//        String fullNameUp = lastNameUp + firstNameUp + middleNameUp;
-//
-//        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUp);
-//
-//        //**Задание 3**
-//        fullNameUp = fullNameUp.replace(" ", "; ");
-//        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUp);
-//
-//        //**Задание 4** Version 1
-//        String fullNameNew = fullName.replace("Ivanov Ivan Ivanovich", "Иванов Семён Семёнович");
-//        fullNameNew = fullNameNew.replace("ё", "е");
-//        System.out.println("Данные ФИО сотрудника — " + fullNameNew);
-//
-//        //**Задание 4** Version 2
-//        String fullNameNew1 = "Иванов Семён Семёнович";
-//        String[] letterChange = fullNameNew1.split(" ");
-//        System.out.print("Данные ФИО сотрудника — ");
-//        for (String letter : letterChange) {
-//            if (letter.contains("ё")) {
-//                letter = letter.replace('ё', 'е');
-//            }
-//            System.out.print(letter + " ");
-//        }
+
+
+
+
+
+
+
+
+
 }
 }
 
